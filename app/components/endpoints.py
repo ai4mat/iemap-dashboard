7d0431e92fe1e17c756f0039beaa38e1a8664a86
SergioEanX <sergio.ferlito@enea.it>
2022-10-03 14:47:40 +0200

    added post_file endpoint

--- app/components/endpoints.py
+++ app/components/endpoints.py
@@ -1,5 +1,7 @@
-class urls():
+class urls:
     base_url = "https://ai4mat.enea.it"
     register = f"{base_url}/auth/register"
     login = f"{base_url}/auth/jwt/login"
     post_metadata = f"{base_url}/api/v1/project/add"
+    post_file = f"{base_url}/api/v1/project/add/file/?project_id="
+    # "633ad6ef7550f1c6479b4e5d"

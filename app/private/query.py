35f860989446239e9bbeba46862a825146b0d156
mpuccini <marcopuccini@protonmail.com>
2022-10-05 14:09:19 +0200

    partial fixes on query.py

--- app/private/query.py
+++ app/private/query.py
@@ -1,8 +1,9 @@
 import streamlit as st
-import pandas as pd
 
 
 def get_df(response):
+    import pandas as pd
+    
     list_doc = []
     get_file_info = lambda x: x["name"] + "." + x["extention"] + " (" + x["size"] + ")"
     get_name_value = lambda x: x["name"] + ":" + str(x["value"])
@@ -37,29 +38,22 @@ def get_df(response):
     return df
 
 
-def on_click(nr, nc):
-
-    from app.components.endpoints import urls
-
-    # import numpy as np
+def on_click():
+    from components.endpoints import urls
     import requests
 
     response = requests.get(urls.query)
     df = get_df(response)
-    # pd.DataFrame(
-    #     np.random.randn(nr, nc),
-    #     columns=('col %d' % i for i in range(nc)))
+
     st.dataframe(df)
+
 
 
 st.title("Query DB")
 st.markdown("Click button to query DB and get all data")
 
-n_row = st.number_input("Number of rows", 1, 100, 10)
-n_col = st.number_input("Number of columns", 1, 100, 10)
-choose = st.button("Query")
-if choose:
-    on_click(n_row, n_col)
+if st.button("Query"):
+    on_click()
 
 
 # https://discuss.streamlit.io/t/how-to-set-page-config-default-layout-to-wide-without-calling-set-page-config/13872/2

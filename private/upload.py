02c8c1c3adbd7dd3b05f34af0e2462802e68696e
mpuccini <marcopuccini@protonmail.com>
2022-11-17 09:36:01 +0100

    upd: laod multiple files

--- private/upload.py
+++ private/upload.py
@@ -32,47 +32,47 @@ if st.button("Load Metadata"):
 st.write("\n")
 
 st.header("2- Upload data file")
-uploaded_file = st.file_uploader("Upload your data file")
-if uploaded_file is not None:
+uploaded_files = st.file_uploader("Upload your data file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
     st.text(
         "File name: {}\nType: {}\nSize: {}".format(
             uploaded_file.name, uploaded_file.type, uploaded_file.size
         )
     )
-if st.button("Load Data") and uploaded_file != None:
+if st.button("Load Data") and uploaded_files != None:
+    for uploaded_file in uploaded_files:
+        # f = BytesIO(uploaded_file.getvalue())
+        with BytesIO(uploaded_file.getvalue()) as f:
+            file_data = f.read()
+            st.session_state["file_to_upload"] = file_data
+            # st.session_state["proj_id"] = "633ad6ef7550f1c6479b4e5d"
 
-    # f = BytesIO(uploaded_file.getvalue())
-    with BytesIO(uploaded_file.getvalue()) as f:
-        file_data = f.read()
-        st.session_state["file_to_upload"] = file_data
-        # st.session_state["proj_id"] = "633ad6ef7550f1c6479b4e5d"
-
-        m = MultipartEncoder(
-            fields={
-                "file": (
-                    uploaded_file.name,
-                    st.session_state.file_to_upload,
-                    uploaded_file.type,
-                )
+            m = MultipartEncoder(
+                fields={
+                    "file": (
+                        uploaded_file.name,
+                        st.session_state.file_to_upload,
+                        uploaded_file.type,
+                    )
+                }
+            )
+            headers = {
+                "Authorization": "Bearer " + st.session_state["token"],
+                "Content-Type": m.content_type,
             }
-        )
-        headers = {
-            "Authorization": "Bearer " + st.session_state["token"],
-            "Content-Type": m.content_type,
-        }
-        proj_data_response = requests.post(
-            urls.post_file
-            + st.session_state["proj_id"]
-            + "&file_name="
-            + uploaded_file.name,
-            headers=headers,
-            data=m,
-        )
-        if proj_data_response.status_code == 200:
-            st.success("Data uploaded successfully!")
-            st.text(proj_data_response.text)
-        elif proj_data_response.status_code == 422:
-            st.error("Metadata not uploaded. Validation error.")
-            st.text(proj_data_response.json())
-        else:
-            st.error("Metadata upload failed!")
+            proj_data_response = requests.post(
+                urls.post_file
+                + st.session_state["proj_id"]
+                + "&file_name="
+                + uploaded_file.name,
+                headers=headers,
+                data=m,
+            )
+            if proj_data_response.status_code == 200:
+                st.success("Data uploaded successfully!")
+                st.text(proj_data_response.text)
+            elif proj_data_response.status_code == 422:
+                st.error("Metadata not uploaded. Validation error.")
+                st.text(proj_data_response.json())
+            else:
+                st.error("Metadata upload failed!")

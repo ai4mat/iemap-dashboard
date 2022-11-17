e84e6f6be80846b870c58b90a78118dd33e74912
mpuccini <marcopuccini@protonmail.com>
2022-11-17 10:55:55 +0100

    upd: video tutorial for user registration added

--- public/home.py
+++ public/home.py
@@ -3,12 +3,18 @@ import streamlit as st
 
 st.write("# Welcome to IEMAP ⚛️")
 
+video_file = open('files/IEMAP-HowToRegister.mov', format='video/mov' 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
+
 
 st.markdown(
     """
     This is a small *PoC* of **IEMAP Platform**. It includes just the _register_, _login_ and _upload_ features. It also includes a _query_ feature, but it is not fully implemented yet.
     
     ### Register page
+
     To register a new user, you need to fill the form with your email and password. The password must have at least 8 characters, one uppercase letter, one lowercase letter and one number.
     Once the user is registerd, it will receive an ativation email. The user must click on the link to activate the account.
     
@@ -17,9 +23,12 @@ st.markdown(
     ### Login page
     You can login with the email and password. If the login is successful, you will be redirected to the upload data page.
     
+    ## Authenticated users only!
     ### Upload data page
-    > You can access this page only if you are logged in!!
-    In this page you can upload a json file with the metadata of the data. The file must have the following structure: 
+    In this page you can upload the metadata json file and project related files. 
+    
+    ### Query data page
+    Just a simple query to the database to get all the data.
     
 """
 )

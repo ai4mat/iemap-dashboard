87dc1fe53eddb6e5bdba611add94cb2f3ed84acd
mpuccini <marcopuccini@protonmail.com>
2022-11-21 14:26:16 +0100

    fix: login fixed + queries visualization

--- private/query.py
+++ private/query.py
@@ -2,27 +2,29 @@ import streamlit as st
 
 
 
-def get_date(datestring):
-    from datetime import datetime 
-    if datestring == None:
-        return None
-    else:
-        return datetime.strptime(datestring, "%Y-%m-%d")
+# def get_date(datestring):
+#     from datetime import datetime 
+#     if datestring == None:
+#         return None
+#     else:
+#         return datetime.fromisoformat(datestring)
 
 def get_df(response):
     import pandas as pd
+    from datetime import datetime
     
     list_doc = []
     get_file_info = lambda x: x["name"] + "." + x["extention"] + " (" + x["size"] + ")"
     get_name_value = lambda x: x["name"] + ":" + str(x["value"])
     for doc in response.json():
         provenance = doc["provenance"]
+        dt = datetime.fromisoformat(provenance["createdAt"])
         isExperiment = bool(doc["process"]["isExperiment"])
         current_doc = {
             "iemap_id": doc.get("iemap_id", None),
     #        "email": provenance["email"],
             "affiliation": provenance["affiliation"],
-            "created at": provenance.get("createdAt", None),
+            "created at": dt.date(),
             "project": doc["project"]["name"],
             "process": "Experiment" if isExperiment else "Computation",
             "method": doc["process"].get("method", None),
@@ -56,7 +58,7 @@ def get_df2(response):
             "iemap_id": doc.get("iemap_id", None),
             "project": doc.get("project_name", None),
             "formula": doc.get("material", None),
-            "created at": dt
+            "created at": dt.date()
             }
         list_doc.append(current_doc)
     df = pd.DataFrame(list_doc)
@@ -73,31 +75,33 @@ tab1, tab2 = st.tabs(["All Projects", "Your Projects"])
 
 with tab1:
     st.header("All Projects")
-    if st.button("Get all data"):
-        from components.endpoints import urls
-        import requests
+    with st.spinner("Loading data..."):
+        if st.button("Get all data"):
+            from components.endpoints import urls
+            import requests
 
-        response = requests.get(urls.query)
-        if response.status_code == 200:
-            df = get_df(response)
-            st.dataframe(df, use_container_width=True)
-        else:
-            st.write(f"An error occurred!")
+            response = requests.get(urls.query)
+            if response.status_code == 200:
+                df = get_df(response)
+                st.dataframe(df, use_container_width=True)
+            else:
+                st.write(f"An error occurred!")
 
 with tab2:
-    st.header("Your Projects")        
-    if st.button("Get your data"):
-        from components.endpoints import urls
-        import requests
+    st.header("Your Projects") 
+    with st.spinner("Loading data..."):       
+        if st.button("Get your data"):
+            from components.endpoints import urls
+            import requests
 
-        response = requests.get(
-            urls.get_user_projects_info,
-            headers={"Authorization": f"Bearer "+st.session_state["token"]}
-        )
-        if response.status_code == 200:           
-            df = get_df2(response)
-            st.dataframe(df, use_container_width=True)
-        else:
-            st.write(f"An error occurred!")
+            response = requests.get(
+                urls.get_user_projects_info,
+                headers={"Authorization": f"Bearer "+st.session_state["token"]}
+            )
+            if response.status_code == 200:           
+                df = get_df2(response)
+                st.dataframe(df, use_container_width=True)
+            else:
+                st.write(f"An error occurred!")
 
 
--- public/login.py
+++ public/login.py
@@ -1,38 +1,36 @@
-#import email
 import streamlit as st
-from components.endpoints import urls
-import requests
-
-# Auxiliary functions for autentication
-def on_click():
+def login(email, pswd):
     import time
+    from components.endpoints import urls
+    import requests
+
+    try:
+        jwt_response = requests.post(
+            urls.login, data={"username": email, "password": pswd}
+        )
+        if jwt_response.status_code == 200:
+            token = jwt_response.json()["access_token"]
+            st.session_state["correct_password"] = True
+            st.session_state["token"] = token
+        else:
+            st.session_state["correct_password"] = False
+    except Exception as e:
+        st.exception(e)
+        mkd = """Please set the user and password."""
+        st.error(mkd)
     if st.session_state["correct_password"]:
         st.session_state["admin_view"] = True
-        time.sleep(.25)
+        time.sleep(0.25)
         query_dict = {}
-        st.experimental_set_query_params(**query_dict)        
+        st.experimental_set_query_params(**query_dict)
     else:
         st.warning("Wrong username or password")
 
 
 st.title("Login")
-c1, c2, c3 = st.columns([2,2,1])
+c1, c2, c3 = st.columns([2, 2, 1])
 email = c1.text_input("Username:", "")
 pswd = c2.text_input("Password:", "", type="password")
-
-try:
-    c3.markdown("")
-    c3.markdown("")
-    c3.button("Login", on_click=on_click)
-    jwt_response = requests.post(urls.login, data={"username": email, "password": pswd})
-    if jwt_response.status_code == 200:
-        token = jwt_response.json()['access_token']
-        st.session_state["correct_password"] = True
-        st.session_state["token"] = token
-    else:
-        st.session_state["correct_password"] = False
-except Exception as e:
-    st.exception(e)
-    mkd = """Please set the user and password."""
-    st.error(mkd)
-
+c3.markdown("")
+c3.markdown("")
+c3.button("Login", on_click=login, args=(email, pswd))

c197241997c6cf7dc76e010ea93734445b265163
mpuccini <marcopuccini@protonmail.com>
2023-03-20 16:10:23 +0100

    fix: new baseurl

--- components/endpoints.py
+++ components/endpoints.py
@@ -1,5 +1,5 @@
 class urls:
-    base_url = "https://ai4mat.enea.it"
+    base_url = "https://iemap.enea.it"
     register = f"{base_url}/auth/register"
     login = f"{base_url}/auth/jwt/login"
     post_metadata = f"{base_url}/api/v1/project/add"
